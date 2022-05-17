21cb101d798b222588587dcdddf48c608c0fb122
Gabriel Quevedo <gabriel.quevedo.c@gmail.com>
2022-05-16 23:22:38 -0400

    Create Initial Structure

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { GlobalStyle } from "./GlobalStyles";
+
+export const App = () => (
+    <>
+    <GlobalStyle />
+    <h1>Componente App</h1>
+    </>
+    )
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
+const path = require('path')
+
+module.exports = {
+  output: {
+    filename: "app.bundle.js",
+    publicPath: '/',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      favicon: path.resolve('./src/assets/icon.png'),
+    }),
+    new WebpackPwaManifestPlugin({
+      name: 'Trainer System - Sistema de Gestión de Capacitaciones',
+      shortname: 'Trainer_sys',
+      description: 'Sistema de gestión de Capacitaciones Wisetrack',
+      background_color: '#fff',
+      theme_color: '#b1a',
+      icons: [
+        {
+          src: path.resolve('./src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512]
+        }
+      ]
+    })
+  ],
+  mode: "development",
+  performance: {
+    hints: false,
+  },
+  devtool: "eval-cheap-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+};
